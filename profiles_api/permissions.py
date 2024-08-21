3180814b7cb02278865e7e6c86f387098fe8577b
Ihor Yatsyshyn <ihoryatsyshyn4@gmail.com>
2024-08-21 16:27:18 +0300

    Added user profile feed API

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -10,3 +10,14 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
 
         return obj.id == request.user.id
+
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allows to update status"""
+
+    def has_object_permission(self, request, view, obj):
+        """Check user trying to edit their status"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id == request.user.id
